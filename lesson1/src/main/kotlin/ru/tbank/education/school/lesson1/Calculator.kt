1646bcbca3731a7ec113961c8ea400c0dc89d061
SeverL626 <alexlizard00@mail.ru>
2025-11-06 16:13:51 +0300

    save

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,18 +1,74 @@
 package ru.tbank.education.school.lesson1
+import kotlin.math.*
 
-/**
- * Метод для вычисления простых арифметических операций.
- */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+enum class OperationType {
+    ADD, SUBTRACT, MULTIPLY, DIVIDE
 }
 
-/**
- * Функция вычисления выражения, представленного строкой
- * @return результат вычисления строки или null, если вычисление невозможно
- * @sample "5 * 2".calculate()
- */
+fun calculate(a: Double, b: Double, operation: OperationType = OperationType.ADD): Double? {
+    when (operation) {
+        OperationType.ADD -> return a + b
+        OperationType.SUBTRACT -> return a - b
+        OperationType.MULTIPLY -> return a * b
+        OperationType.DIVIDE -> {
+            if (b != 0.0) {
+                return a / b
+            } else {
+                return null
+            }
+        }
+    }
+}
+
+fun HardCalculate(operation: String, a: Double): Double? {
+    when (operation) {
+        "sin" -> return sin(a)
+        "cos" -> return cos(a)
+        "sqrt" -> {
+            if (a > 0.0) {
+                return sqrt(a)
+            } else {
+                return null
+            }
+        }
+    }
+}
+
+fun OperatorClassType()
+
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val x = this.trim().replace("(", "").replace(")", "").split(" ")
+    if (x.size == 3) {
+        val calc_flag = true
+    } else if (x.size == 2) {
+        val sqrt_flag = true
+    } else {
+        return null
+    }
+
+
+
+
+
+
+    val a = x[0].toDoubleOrNull()
+    val b = x[2].toDoubleOrNull()
+    if (a == null || b == null) {
+        return null
+    }
+
+    val op: OperationType =
+        if (x[1] == "+") {
+        OperationType.ADD
+    } else if (x[1] == "-") {
+        OperationType.SUBTRACT
+    } else if (x[1] == "*") {
+        OperationType.MULTIPLY
+    } else if (x[1] == "/") {
+        OperationType.DIVIDE
+    } else {
+        return null
+    }
+    return calculate(a, b, op)
 }
--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -4,5 +4,15 @@ package ru.tbank.education.school.lesson1
  * Сумма четных чисел.
  */
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var sum = 0
+    for (i in numbers) {
+        if (i % 2 == 0) {
+            sum += i
+        }
+    }
+    return sum
+}
+// пример работы
+fun main() {
+    print(sumEvenNumbers(arrayOf(1, 2, 3, 4, 5)))
 }
