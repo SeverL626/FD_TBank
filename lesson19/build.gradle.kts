abc3013b5d1b601606d46e8c9d6592b666b12595
SeverL626 <alexlizard00@mail.ru>
2026-04-02 11:30:54 +0300

    set les19 final

--- lesson19/build.gradle.kts
+++ lesson19/build.gradle.kts
@@ -1,22 +1,12 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
+    buildlogic.`kotlin-common-conventions-no-detekt`
+
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
-
-    id("org.jetbrains.kotlin.jvm")
-    id("org.jetbrains.kotlin.plugin.spring")
-    id("org.jetbrains.kotlin.plugin.jpa")
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
 
 repositories {
     mavenCentral()
@@ -37,13 +27,18 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = freeCompilerArgs + "-Xjsr305=strict"
-        jvmTarget = "17"
-    }
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs(
+        "-Dfile.encoding=UTF-8",
+        "-Dsun.stdout.encoding=UTF-8",
+        "-Dsun.stderr.encoding=UTF-8"
+    )
 }
 
-tasks.withType<Test> {
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
